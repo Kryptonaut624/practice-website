321dc13fe49acb85c212a2c984deff8eb64c1fd6
Danny Perego <p0602221@students.katyisd.org>
2018-10-02 08:31:17 -0500

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -1,21 +1,25 @@
 calculate.on('click', event =>{
   const val= input.value();
   const eq= false;
+  const equation;
   const find;
   const alph='abcdefghijklmnopqrstuvwxyz';
   const answer;
+  //finds if is equation, then finds variable
   for(let i=0;i<val.length;i++){
     if(val.indexOf(i)==='='){
       eq=true;
-    }
-  }
-  if(eq===true){
-    for(let v=0;v<val.length;v++){
-      for(let letter=0;letter<alph.length;letter++){
-        if(val.substring(v,v+1)===alph.substring(letter,letter+1){
-          find=alph.substring(letter,letter+1);
+      for(let v=0;v<val.length;v++){
+        for(let letter=0;letter<alph.length;letter++){
+          if(val.substring(v,v+1)===alph.substring(letter,letter+1){
+            find=alph.substring(letter,letter+1);
+          }
         }
       }
-    }
+      str=val.split('=');
+    }//end of if statement
+  }
+  if(eq===false){
+    answer=val.valueOf();
   }
 }
