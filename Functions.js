d0d3a5d90359b38adfcf4e73ab0e5c551bfdf004
Danny Perego <p0602221@students.katyisd.org>
2018-10-04 08:29:53 -0500

    Update Functions.js

--- Functions.js
+++ Functions.js
@@ -1,25 +1,29 @@
-export function calc=(val)=>{
+const inputField= document.querySelector('#input');
+const calculate= document.querySelector('#calculate');
+const responseField= document.querySelector('#responseField');
+
+const calc=()=>{
   const eq= false;
   const equation;
   const find;
   const alph='abcdefghijklmnopqrstuvwxyz';
   const answer;
   //finds if is equation, then finds variable
-  for(let i=0;i<val.length;i++){
-    if(val.indexOf(i)==='='){
+  for(let i=0;i<inputField.length;i++){
+    if(inputField.indexOf(i)==='='){
       eq=true;
-      for(let v=0;v<val.length;v++){
+      for(let v=0;v<inputField.length;v++){
         for(let letter=0;letter<alph.length;letter++){
-          if(val.substring(v,v+1)===alph.substring(letter,letter+1){
+          if(inputField.substring(v,v+1)===alph.substring(letter,letter+1){
             find=alph.substring(letter,letter+1);
           }
         }
       }
-      equation=val.split('=');
+      equation=inputField.split('=');
     }//end of if statement
   }
   if(eq===false){
-    answer=val.valueOf();
+    answer=inputField.valueOf();
     responseField.innerHTML=answer;
   }
 }
